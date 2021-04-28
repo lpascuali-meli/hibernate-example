c45d2eb20f876043a1c8adaed643ddd43aa78b63
Ignacio Monzalvo <ignacio.monzalvo@mercadolibre.com>
2021-04-28 17:35:04 -0300

    Modify AppointmentDto

--- hibernate/src/main/java/meli/bootcamp/hibernate/dtos/AppointmentDto.java
+++ hibernate/src/main/java/meli/bootcamp/hibernate/dtos/AppointmentDto.java
@@ -1,14 +1,24 @@
 package meli.bootcamp.hibernate.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentDto {
-    private Long dentistId;
-    private Long patientId;
+    private Long dentist;
+    private Long patient;
+    @JsonFormat(pattern="dd/MM/yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate date;
 }
